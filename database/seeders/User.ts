6b9389516ec34888b7b261f958737f062e5f31be
Gabriel Caetano <gabrielvscaetano@gmail.com>
2022-11-28 08:54:04 -0300

    first commit

---
+++ database/seeders/User.ts
@@ -0,0 +1,11 @@
+import BaseSeeder from '@ioc:Adonis/Lucid/Seeder'
+import User from 'App/Models/User'
+import Env from '@ioc:Adonis/Core/Env'
+export default class UserSeeder extends BaseSeeder {
+  public async run() {
+    await User.create({
+      email: Env.get('ADMIN_EMAIL'),
+      password: Env.get('ADMIN_PASSWORD'),
+    })
+  }
+}
